53c0664abbb027d0b4223329dd0a888bd989f156
Caranille <mycaranille@gmail.com>
2018-02-09 16:02:46 +0100

    fix

--- administration/html/header.php
+++ administration/html/header.php
@@ -37,7 +37,7 @@ require_once("../../kernel/place/index.php");
 
     <body>
 		<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-			<a class="navbar-brand" href="../../modules/main/index.php">Panel Administration</a>
+			<a class="navbar-brand" href="index.php">Panel Administration</a>
 			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
 				<span class="navbar-toggler-icon"></span>
 			</button>
