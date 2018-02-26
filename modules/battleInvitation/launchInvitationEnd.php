f3734683cd7f1578ac8e790632d5d2d1484f2865
Caranille <mycaranille@gmail.com>
2018-02-26 08:12:47 +0100

    Update launchInvitationEnd.php

--- modules/battleInvitation/launchInvitationEnd.php
+++ modules/battleInvitation/launchInvitationEnd.php
@@ -2,8 +2,8 @@
 
 //S'il n'y a aucune session c'est que le joueur n'est pas connecté alors on le redirige vers l'accueil
 if (empty($_SESSION['account'])) { exit(header("Location: ../../index.php")); }
-//Si le joueur n'a pas les droits administrateurs (Accès 2) on le redirige vers l'accueil
-if ($accountAccess < 2) { exit(header("Location: ../../index.php")); }
+//S'il y a actuellement un combat on redirige le joueur vers le module battle
+if ($battleRow > 0) { exit(header("Location: ../../modules/battle/index.php")); }
 
 //Si les variables $_POST suivantes existent
 if (isset($_POST['battleInvitationCharacterId'])
