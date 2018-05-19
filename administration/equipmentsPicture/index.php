312dd50f05d787bf5bee42a980de25e9f4e4d875
caranille <mycaranille@gmail.com>
2018-05-19 15:41:12 +0200

    fix

--- administration/equipmentsPicture/index.php
+++ administration/equipmentsPicture/index.php
@@ -6,6 +6,36 @@ if (empty($_SESSION['account'])) { exit(header("Location: ../../index.php")); }
 //Si le joueur n'a pas les droits administrateurs (Accès 2) on le redirige vers l'accueil
 if ($accountAccess < 2) { exit(header("Location: ../../index.php")); }
 ?>
+    
+<form method="POST" action="manageEquipmentsPicture.php">
+    Images d'équipements : <select name="pictureFile" class="form-control">
+            
+        <?php
+        $dir = '../../img/equipments/';
+        //On ouvre le dossier
+        if ($dh = opendir($dir)) 
+        {
+            //On fait une boucle sur chaque fichier
+            while (($file = readdir($dh)) !== false) 
+            {
+                //On affiche chaque fichier dans un menu option
+                if($file != '.' && $file != '..') 
+                {
+                    ?>
+                    <option value="<?php echo $file ?>"><?php echo $file; ?></option>
+                    <?php
+                }
+            }
+            // on ferme la connection
+            closedir($dh);
+        }
+        ?>
+
+    </select>
+    <input type="submit" name="manage" class="btn btn-default form-control" value="Gérer l'image">
+</form>
+
+<hr>
 
 <form method="POST" action="addEquipmentsPicture.php" enctype="multipart/form-data">
     <!-- On limite le fichier à 1000Ko -->
