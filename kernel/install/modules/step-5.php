4e0a2e249a7a8afbc537d2aba5d08db7029ae3e6
Caranille <mycaranille@gmail.com>
2018-06-23 11:49:19 +0200

    fix

--- kernel/install/modules/step-5.php
+++ kernel/install/modules/step-5.php
@@ -145,36 +145,36 @@ if (isset($_POST['accountPseudo'])
                             '0', //characterMpEquipments
                             '0', //characterMpGuild
                             '10', //characterMpTotal
-                            '2', //characterStrength
+                            '1', //characterStrength
                             '0', //characterStrengthSkillPoints
                             '0', //characterStrengthParchment
                             '0', //characterStrengthEquipments
                             '0', //characterStrengthGuild
-                            '2', //characterStrengthTotal
-                            '2', //characterMagic
+                            '1', //characterStrengthTotal
+                            '1', //characterMagic
                             '0', //characterMagicSkillPoints
                             '0', //characterMagicParchment
                             '0', //characterMagicEquipments
                             '0', //characterMagicGuild
-                            '2', //characterMagicTotal
-                            '2', //characterAgility
+                            '1', //characterMagicTotal
+                            '0', //characterAgility
                             '0', //characterAgilitySkillPoints
                             '0', //characterAgilityParchment
                             '0', //characterAgilityEquipments
                             '0', //characterAgilityGuild
-                            '2', //characterAgilityTotal
-                            '2', //characterDefense
+                            '0', //characterAgilityTotal
+                            '0', //characterDefense
                             '0', //characterDefenseSkillPoints
                             '0', //characterDefenseParchment
                             '0', //characterDefenseEquipments
                             '0', //characterDefenseGuild
-                            '2', //characterDefenseTotal
-                            '2', //characterDefenseMagic
+                            '0', //characterDefenseTotal
+                            '0', //characterDefenseMagic
                             '0', //characterDefenseMagicSkillPoints
                             '0', //characterDefenseMagicParchment
                             '0', //characterDefenseMagicEquipments
                             '0', //characterDefenseMagicGuild
-                            '2', //characterDefenseMagicTotal
+                            '0', //characterDefenseMagicTotal
                             '0', //characterWisdom
                             '0', //characterWisdomSkillPoints
                             '0', //characterWisdomParchment
